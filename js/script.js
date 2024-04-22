1c57b16b28199994a691de8ed43cc5e59fcad3c9
FabioGMarascia <fabiogaspare.marascia@gmail.com>
2024-04-22 15:52:33 +0200

    fix and bonus

--- js/script.js
+++ js/script.js
@@ -1,9 +1,15 @@
 const email_list = [`fabio@gmail.com`, `silvia@gmail.com`, `martina@gmail.com`];
 const user_email = prompt(`Inserisci la tua e-mail`);
-let negative_result;
+let result = 0;
 
 for (let i = 0; i < email_list.length; i++) {
 	if (user_email == email_list[i]) {
-		alert(`È presente`);
+		result = 1;
 	}
+}
+
+if (result == 1) {
+	alert(`È presente`);
+} else {
+	alert(`Non è presente`);
 }
