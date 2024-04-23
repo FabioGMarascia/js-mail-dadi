54a721c65cf9c6b14b6d8e8f339323cace1f2f89
FabioGMarascia <fabiogaspare.marascia@gmail.com>
2024-04-23 09:38:21 +0200

    Boolean value

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 const email_list = [`fabio@gmail.com`, `silvia@gmail.com`, `martina@gmail.com`];
 const user_email = prompt(`Inserisci la tua e-mail`);
-let result = 0;
+let result = false;
 
 for (let i = 0; i < email_list.length; i++) {
 	if (user_email == email_list[i]) {
-		result = 1;
+		result = true;
 	}
 }
 
-if (result == 1) {
+if (result == true) {
 	alert(`È presente`);
 } else {
 	alert(`Non è presente`);
